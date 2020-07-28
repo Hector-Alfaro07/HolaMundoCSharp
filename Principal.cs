8a3fac93511fce5244fc5dd39d6a51e63de2052d
Héctor Alfaro <hector.alfaro.perry07@gmail.com>
2020-07-28 18:15:10 -0500

    Agregamos la variable sueldo y la imprimimos

--- Principal.cs
+++ Principal.cs
@@ -6,8 +6,10 @@ class Principal{
 
 string programador1 = "Hector Alfaro";
 int edad1 = 25;
+double sueldo1 = 2000;
         Console.WriteLine("Hola mundo");
         Console.WriteLine("\nNombre: " + programador1);
         Console.WriteLine("\nEdad: " + edad1);
+        Console.WriteLine("\nSueldo: " + sueldo1);
     }
 }
